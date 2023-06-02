d657e2cf4ec209143a5e6f24bf7937d3697a5aa7
AmeriCodes <mateus-americo@live.com>
2023-06-02 16:14:09 -0300

    understanding print command

--- basic_python/class2.py
+++ basic_python/class2.py
@@ -1,8 +1,10 @@
 """
-A função 'print' exibe algo na tela, ela pode acumular vários argumentos, é só separa-los por vírgula, por padão, o python vai dar um espaço entre os argumentos, mas isso pode ser mudado utilizando o comando 'sep="?"', o que for colocado dentro das aspas será utilizado como separador; e o comando end='' serve para quebrar linhas.
+The 'print' function displays something on the screen. It can accumulate multiple arguments by separating them with commas. By default, Python will add a space between the arguments, but this can be changed using the 'sep="?"' command, where whatever is placed inside the quotation marks will be used as a separator. The 'end=''' command is used to prevent line breaks.
 """
 
 # \r\n = CRLF
+print(52, 40)
 print(12, 34, 1011, sep="-", end='##\r\n')
 print(56, 78, sep='-', end='\n')
+print(52, 44, end='') # Here, since there is nothing between the quotation marks, Python does not break the line and continues with the next command.
 print(9, 10, sep= '&&')
