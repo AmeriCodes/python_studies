df8028b662a793c2ce8fb8eb64e66968ad6262e3
AmeriCodes <mateus-americo@live.com>
2023-06-05 09:03:05 -0300

    bolean type

---
+++ basic_python/class5.py
@@ -0,0 +1,9 @@
+"""
+In addition to integers and floats, another commonly used primitive type is the boolean type. The boolean type represents a logical value and can have two possible values: True or False. Booleans are often used in conditional statements and logical operations to control the flow of a program.
+"""
+print(10 == 10)
+print(10 == 11)
+print(type(True))
+print(type(False))
+print(type(10 == 10))
+print(type(10 == 11))
