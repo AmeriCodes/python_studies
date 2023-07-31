bdce67727a7039f7852bc4d1a0c26e5c3b3a3d47
AmeriCodes <mateus-americo@live.com>
2023-07-31 09:17:46 -0300

    aprendendo a usar o lvim

--- python_brasil/sequential_structure/ex05.py
+++ python_brasil/sequential_structure/ex05.py
@@ -1,3 +1,28 @@
-#
+"""
 Faça um Programa que converta metros para centímetros.
-#
+"""
+
+def titulo(msg):
+    tam = len(msg) + 10
+    print('~' * tam)
+    print(f'     {msg}')
+    return print('~' * tam)
+
+
+titulo('CONVERSÃO PARA CM')
+
+
+while True:
+    entrada = input("Digite o valor em metros: ")
+    try:
+        entrada = float(entrada)
+        break
+    except ValueError:
+        print('Digite em metros.')
+        continue
+
+
+conversao = entrada * 100
+
+print(f'Resulta em {conversao:.0f}Cm')
+
--- python_brasil/sequential_structure/ex06.py
+++ python_brasil/sequential_structure/ex06.py
@@ -1,3 +1,27 @@
-#
+"""
 Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
-#
+"""
+
+def titulo(msg):
+    tam = len(msg) + 10
+    print('~' * tam)
+    print(f'     {msg}')
+    print('~' * tam)
+    return ''
+
+pi = 3.14
+
+print(titulo('ÁREA DO CÍRCULO'))
+
+while True:
+    try:
+        raio = float(input('Digite o raio do círculo[m]: '))
+        break
+    except ValueError:
+        print('Entrada inválida.')
+        continue
+
+area = pi * raio ** 2
+
+print(f'De acordo com o raio fornecido\nA área desse círculo seria aproximadamente {area:.2f}m²')
+
