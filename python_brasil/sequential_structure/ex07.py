d3946cf55e217307233492c5dab3ec1905ccf5f1
AmeriCodes <mateus-americo@live.com>
2023-08-02 16:49:16 -0300

    dobro da área do quadrado

--- python_brasil/sequential_structure/ex07.py
+++ python_brasil/sequential_structure/ex07.py
@@ -1,3 +1,23 @@
-#
-Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
-#
+"""
+Faça um Programa que calcule a área de um quadrado,
+em seguida mostre o dobro desta área para o usuário.
+"""
+
+while True:
+    try:
+        lado = int(input("Digita a medida de um lado do quadrado: "))
+        if lado > 0:
+            break
+        else:
+            print("(Entrada inválida!)\nDigite apenas números positivos.")
+            continue
+    except ValueError:
+        print("(Entrada inválida!)\nDigite apenas números inteiros.")
+        continue
+
+
+area = lado**2
+
+print("-" * 30)
+print(f"A área do quadrado seria {area}².")
+print(f"O dobro da área desse quadrado seria {area * 2}².")
