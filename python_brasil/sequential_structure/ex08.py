973043a9e8857ff3607a32402b269bb6bc0d845e
AmeriCodes <mateus-americo@live.com>
2023-08-05 12:11:14 -0300

    salário mensal

--- python_brasil/sequential_structure/ex08.py
+++ python_brasil/sequential_structure/ex08.py
@@ -1,3 +1,29 @@
-#
-Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês.
-#
+"""
+Faça um Programa que pergunte quanto você ganha por hora
+e o número de horas trabalhadas no mês.
+Calcule e mostre o total do seu salário no referido mês.
+"""
+
+
+def salario_mes(salario, horas):
+    return f"{salario * horas:.2f}"
+
+
+while True:
+    try:
+        money = float(input("Salario por hora: "))
+        break
+    except ValueError:
+        print("Entrada inválida.")
+    continue
+
+while True:
+    try:
+        worked_hours_this_month = float(input("Horas trabalhadas no mês: "))
+        break
+    except ValueError:
+        print("Entrada inválida.")
+    continue
+
+
+print(salario_mes(money, worked_hours_this_month))
