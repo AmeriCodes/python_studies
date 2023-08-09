ddd8c5213159157fac944ca345239dca34e21417
AmeriCodes <mateus-americo@live.com>
2023-08-09 18:06:51 -0300

    Verificando sexo / Fº to Cº

--- python_brasil/decision_structure/ex03.py
+++ python_brasil/decision_structure/ex03.py
@@ -1,3 +1,20 @@
-#
+"""
 Faça um Programa que verifique se uma letra digitada é "F" ou "M". Conforme a letra escrever: F - Feminino, M - Masculino, Sexo Inválido.
-#
+"""
+
+def verificando_sexo():
+    while True:
+        try:
+            print('~' * 24)
+            entrada = input('Qual é o seu sexo [M/F]: ').strip()[0]
+            if entrada.upper() == 'F':
+                return 'Feminino'
+            elif entrada.upper() == 'M':
+                return 'Masculino'
+            else:
+                print ('Sexo Inválido\n(M)asculino\n(F)eminino.')
+        except IndexError:
+            print('Não deixe em branco\nDigite algo.')
+
+
+print(verificando_sexo())
--- python_brasil/sequential_structure/ex09.py
+++ python_brasil/sequential_structure/ex09.py
@@ -1,4 +1,28 @@
-#
-Faça um Programa que peça a temperatura em graus Fahrenheit, transforme e mostre a temperatura em graus Celsius.
+"""
+Faça um Programa que peça a temperatura em graus Fahrenheit,
+transforme e mostre a temperatura em graus Celsius.
 C = 5 * ((F-32) / 9).
-#
+"""
+
+
+def titulo(msg):
+    tam = len(msg) + 10
+    print('~' * tam)
+    print(f'     {msg}')
+    return print('~' * tam)
+
+
+titulo('Fahrenheit >>> Celsius')
+while True:
+    try:
+        entry = int(input('Digite os graus Fº: '))
+        break
+    except ValueError:
+        print('Digite apenas números inteiros!')
+        continue
+
+
+celsius = 5 * ((entry - 32) / 9)
+
+
+print(f'{entry}º Fahrenheit são {celsius:.1f}º Celsius.')
